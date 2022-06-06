1bc1a5fcd74ad841807a3acadd04226be763e376
Masataka SUMI <santarh@gmail.com>
2022-06-07 00:33:59 +0900

    周期呼び出しを Task.Delay から PeriodicTimer に変更

--- BlackmagicCameraControl/Program.cs
+++ BlackmagicCameraControl/Program.cs
@@ -21,30 +21,43 @@ namespace BlackmagicCameraControl
             {
                 var blackmagicCameraService = new Guid("291d567a-6d75-11e6-8b77-86f30ca893d3");
                 var outgoingCameraControlCharacter = new Guid("5DD3465F-1AEE-4299-8493-D2ECA2F8E1BB");
-                
+
                 var address = await new BleAdvertisementFinder(blackmagicCameraService).GetBluetoothAddressAsync(token);
-                Console.WriteLine(address.ToString()); // 229589057331041
+                Console.WriteLine(address.ToString());
                 using (var device = await BluetoothLEDevice.FromBluetoothAddressAsync(address))
                 {
                     var services = await device.GetGattServicesForUuidAsync(blackmagicCameraService);
-                    if (services.Status != GattCommunicationStatus.Success) throw new Exception($"Can't get service. {services.Status}");
+                    if (services.Status != GattCommunicationStatus.Success)
+                    {
+                        throw new Exception($"Can't get service. {services.Status}");
+                    }
                     var service = services.Services[0];
 
                     var characters = await service.GetCharacteristicsForUuidAsync(outgoingCameraControlCharacter);
-                    if (characters.Status != GattCommunicationStatus.Success) throw new Exception($"Can't get characteristics. {characters.Status}");
+                    if (characters.Status != GattCommunicationStatus.Success)
+                    {
+                        throw new Exception($"Can't get characteristics. {characters.Status}");
+                    }
                     var character = characters.Characteristics[0];
 
-                    if (!character.CharacteristicProperties.HasFlag(GattCharacteristicProperties.Write)) throw new Exception("Can't write.");
+                    if (!character.CharacteristicProperties.HasFlag(GattCharacteristicProperties.Write))
+                    {
+                        throw new Exception("Can't write.");
+                    }
 
+                    Console.WriteLine($"CONNECTED to {address}");
+
+                    using var timer = new PeriodicTimer(TimeSpan.FromSeconds(1.0 / 30.0));
                     var gamepad = XInput.Wrapper.X.Gamepad_1;
                     while (true)
                     {
-                        await Task.Delay(TimeSpan.FromSeconds(1.0 / 30.0), token);
-                        if (token.IsCancellationRequested) break;
-
-                        gamepad.Update();
                         try
                         {
+                            await timer.WaitForNextTickAsync(token);
+                            if (token.IsCancellationRequested) break;
+
+                            gamepad.Update();
+
                             var messageQueue = new Queue<CameraControlProtocolMessage>();
                             // focus
                             var diff = gamepad.LStick_N.X;
@@ -54,6 +67,7 @@ namespace BlackmagicCameraControl
                                     DestinationDeviceType.One,
                                     diff * 0.02f));
                             }
+
                             if (gamepad.LStick_down)
                             {
                                 messageQueue.Enqueue(CameraControlProtocolMessage.AutoFocus(DestinationDeviceType.One));
@@ -85,8 +99,12 @@ namespace BlackmagicCameraControl
                         }
                     }
                 }
-                
+
                 Console.WriteLine("Done");
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("CANCELED!");
             }
             catch (Exception e)
             {
