a6144d663b6f44fd26178d55702b70923651aa16
Masataka SUMI <santarh@gmail.com>
2022-06-07 02:34:09 +0900

    Refactoring

---
+++ BlackmagicCameraControl/BlackmagicBluetoothCameraControl.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Concurrent;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using BlackmagicCameraControlProtocol;
+
+namespace BlackmagicCameraControl;
+
+public sealed class BlackmagicBluetoothCameraControl : IDisposable
+{
+    public static readonly Guid BlackmagicCameraServiceGuid = new Guid("291d567a-6d75-11e6-8b77-86f30ca893d3");
+    public static readonly Guid OutgoingCameraControlCharacteristicGuid = new Guid("5DD3465F-1AEE-4299-8493-D2ECA2F8E1BB");
+    public static readonly Guid IncomingCameraControlCharacteristicGuid = new Guid("B864E140-76A0-416A-BF30-5876504537D9");
+
+    private readonly BluetoothLEDevice _device;
+    private readonly GattDeviceService _service;
+    private readonly GattCharacteristic _outgoingCharacteristic;
+    private readonly GattCharacteristic _incomingCharacteristic;
+
+    private BlackmagicBluetoothCameraControl(BluetoothLEDevice device, GattDeviceService service,
+        GattCharacteristic outgoingCharacteristic, GattCharacteristic incomingCharacteristic)
+    {
+        _device = device;
+        _service = service;
+        _outgoingCharacteristic = outgoingCharacteristic;
+        _incomingCharacteristic = incomingCharacteristic;
+    }
+
+    public void Dispose()
+    {
+        _service?.Dispose();
+        _device?.Dispose();
+    }
+
+    public async ValueTask<bool> SendAsync(CameraControlProtocolMessage message)
+    {
+        var result = await _outgoingCharacteristic.WriteValueAsync(message.Message.AsBuffer());
+        if (result != GattCommunicationStatus.Success)
+        {
+            Console.WriteLine($"Send Error: {result}");
+        }
+        return result == GattCommunicationStatus.Success;
+    }
+
+    public static async ValueTask<BlackmagicBluetoothCameraControl> CreateAsync(ulong bluetoothAddress, CancellationToken token)
+    {
+        var device = await BluetoothLEDevice.FromBluetoothAddressAsync(bluetoothAddress);
+        token.ThrowIfCancellationRequested();
+
+        var services = await device.GetGattServicesForUuidAsync(BlackmagicCameraServiceGuid);
+        if (services.Status != GattCommunicationStatus.Success)
+        {
+            throw new Exception($"Can't get service. {services.Status}");
+        }
+        var service = services.Services[0];
+        var outgoingCharacteristic = await GetCharacteristicAsync(service, OutgoingCameraControlCharacteristicGuid);
+        var incomingCharacteristic = await GetCharacteristicAsync(service, IncomingCameraControlCharacteristicGuid);
+
+        return new BlackmagicBluetoothCameraControl(device, service, outgoingCharacteristic, incomingCharacteristic);
+    }
+
+
+    private static async ValueTask<GattCharacteristic> GetCharacteristicAsync(GattDeviceService service, Guid guid)
+    {
+        var characters = await service.GetCharacteristicsForUuidAsync(guid);
+        if (characters.Status != GattCommunicationStatus.Success)
+        {
+            throw new Exception($"Can't get characteristics. {characters.Status}");
+        }
+        return characters.Characteristics[0];
+    }
+}
--- BlackmagicCameraControl/Program.cs
+++ BlackmagicCameraControl/Program.cs
@@ -1,115 +1,76 @@
-﻿using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Devices.Bluetooth;
-using Windows.Devices.Bluetooth.GenericAttributeProfile;
-using BlackmagicCameraControlProtocol;
+﻿using BlackmagicCameraControlProtocol;
 
-namespace BlackmagicCameraControl
+namespace BlackmagicCameraControl;
+
+internal static class Program
 {
-    internal static class Program
+    public static async Task Main(string[] args)
     {
-        public static async Task Main(string[] args)
+        var tokenSource = new CancellationTokenSource();
+        Console.CancelKeyPress += (sender, ev) =>
         {
-            var tokenSource = new CancellationTokenSource();
-            Console.CancelKeyPress += (sender, ev) =>
-            {
-                tokenSource.Cancel();
-                ev.Cancel = true;
-            };
-            var token = tokenSource.Token;
+            tokenSource.Cancel();
+            ev.Cancel = true;
+        };
+        var token = tokenSource.Token;
 
-            try
-            {
-                var blackmagicCameraService = new Guid("291d567a-6d75-11e6-8b77-86f30ca893d3");
-                var outgoingCameraControlCharacter = new Guid("5DD3465F-1AEE-4299-8493-D2ECA2F8E1BB");
+        try
+        {
+            var address = await new BleAdvertisementFinder(BlackmagicBluetoothCameraControl.BlackmagicCameraServiceGuid).GetBluetoothAddressAsync(token);
+            using var bluetoothControl = await BlackmagicBluetoothCameraControl.CreateAsync(address, token);
+            Console.WriteLine($"CONNECTED to {address}");
 
-                var address = await new BleAdvertisementFinder(blackmagicCameraService).GetBluetoothAddressAsync(token);
-                Console.WriteLine(address.ToString());
-                using (var device = await BluetoothLEDevice.FromBluetoothAddressAsync(address))
+            using var timer = new PeriodicTimer(TimeSpan.FromSeconds(1.0 / 10.0));
+            var gamepad = XInput.Wrapper.X.Gamepad_1;
+            while (true)
+            {
+                try
                 {
-                    var services = await device.GetGattServicesForUuidAsync(blackmagicCameraService);
-                    if (services.Status != GattCommunicationStatus.Success)
-                    {
-                        throw new Exception($"Can't get service. {services.Status}");
-                    }
-                    var service = services.Services[0];
+                    await timer.WaitForNextTickAsync(token);
+                    if (token.IsCancellationRequested) break;
 
-                    var characters = await service.GetCharacteristicsForUuidAsync(outgoingCameraControlCharacter);
-                    if (characters.Status != GattCommunicationStatus.Success)
-                    {
-                        throw new Exception($"Can't get characteristics. {characters.Status}");
-                    }
-                    var character = characters.Characteristics[0];
+                    gamepad.Update();
 
-                    if (!character.CharacteristicProperties.HasFlag(GattCharacteristicProperties.Write))
+                    // focus
+                    var diff = gamepad.LStick_N.X;
+                    if (Math.Abs(diff) > 0.01f)
                     {
-                        throw new Exception("Can't write.");
+                        await bluetoothControl.SendAsync(CameraControlProtocolMessage.ManualLensFocusRelative(
+                            DestinationDeviceType.One,
+                            diff * 0.02f));
                     }
 
-                    Console.WriteLine($"CONNECTED to {address}");
-
-                    using var timer = new PeriodicTimer(TimeSpan.FromSeconds(1.0 / 30.0));
-                    var gamepad = XInput.Wrapper.X.Gamepad_1;
-                    while (true)
+                    if (gamepad.LStick_down)
                     {
-                        try
-                        {
-                            await timer.WaitForNextTickAsync(token);
-                            if (token.IsCancellationRequested) break;
+                        await bluetoothControl.SendAsync(CameraControlProtocolMessage.AutoFocus(
+                            DestinationDeviceType.One));
+                    }
 
-                            gamepad.Update();
-
-                            var messageQueue = new Queue<CameraControlProtocolMessage>();
-                            // focus
-                            var diff = gamepad.LStick_N.X;
-                            if (Math.Abs(diff) > 0.01f)
-                            {
-                                messageQueue.Enqueue(CameraControlProtocolMessage.ManualLensFocusRelative(
-                                    DestinationDeviceType.One,
-                                    diff * 0.02f));
-                            }
-
-                            if (gamepad.LStick_down)
-                            {
-                                messageQueue.Enqueue(CameraControlProtocolMessage.AutoFocus(DestinationDeviceType.One));
-                            }
-
-                            if (gamepad.Back_down)
-                            {
-                                messageQueue.Enqueue(CameraControlProtocolMessage.StartRecording(DestinationDeviceType.One));
-                            }
-
-                            while (messageQueue.Count > 0)
-                            {
-                                var message = messageQueue.Dequeue();
-                                Console.WriteLine($"Sending... {message}");
-                                var result = await character.WriteValueAsync(message.Message.AsBuffer());
-                                if (result != GattCommunicationStatus.Success)
-                                {
-                                    Console.WriteLine($"Send Error: {result}");
-                                }
-                            }
-                        }
-                        catch (OperationCanceledException)
-                        {
-                            throw;
-                        }
-                        catch (Exception e)
-                        {
-                            Console.WriteLine($"Catch Exception: {e}");
-                        }
+                    if (gamepad.Back_down)
+                    {
+                        await bluetoothControl.SendAsync(CameraControlProtocolMessage.StartRecording(
+                            DestinationDeviceType.One));
                     }
                 }
+                catch (OperationCanceledException)
+                {
+                    throw;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Catch Exception: {e}");
+                }
+            }
 
-                Console.WriteLine("Done");
-            }
-            catch (OperationCanceledException)
-            {
-                Console.WriteLine("CANCELED!");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Catch Exception: {e}");
-            }
+            Console.WriteLine("Done");
+        }
+        catch (OperationCanceledException)
+        {
+            Console.WriteLine("CANCELED!");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Catch Exception: {e}");
         }
     }
 }
